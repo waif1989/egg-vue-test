6ff75e43ff6696a847c4fa0eecdba676ed1709c2
chensiwei <chensiwei@corp.netease.com>
2019-06-21 18:43:04 +0800

    fix: 调试服务器渲染

--- app/web/page/home/home.vue
+++ app/web/page/home/home.vue
@@ -1,9 +1,12 @@
 <template>
 	<layout>
-		<div v-html="message"> {{message}}</div>
+		<div class="aa" v-html="message"> {{message}}</div>
 	</layout>
 </template>
 <style>
+	.aa {
+		color: red;
+	}
 </style>
 <script type="text/babel">
   export default {
